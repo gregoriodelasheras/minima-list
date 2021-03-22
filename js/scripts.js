35ff7ff43d2b0e9eaaeec6fb333fd470242e053f
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-03-22 15:35:12 +0100

    refactor: change alert message to modal message

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,54 @@
 function newItem() {
   // Add a new item to the list
-  let ol = $("#list");
-  let li = $("<li></li>");
-  let inputValue = $("#input").val();
+  let ol = $('#list');
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  let modalContainer = $('#modal-container');
   li.append(inputValue);
 
   // Input Validation
-  inputValue === "" ? showMessage() : ol.append(li);
+  inputValue === '' ? showMessage() : ol.append(li);
 
+  // Modal
   function showMessage() {
-    alert("You must write something!");
+    let modal = $('.modal');
+
+    let closeButtonElement = $('.modal-close');
+    closeButtonElement.on('click', hideModal);
+
+    modalContainer.append(modal);
+
+    modalContainer.addClass('is-visible');
   }
 
+  function hideModal() {
+    modalContainer.removeClass('is-visible');
+  }
+
+  $(window).keydown(function (e) {
+    if (e.key === 'Escape' && modalContainer.hasClass('is-visible')) {
+      e.preventDefault();
+      hideModal();
+    }
+  });
+
+  modalContainer.on('click', function (e) {
+    e.preventDefault();
+    hideModal();
+  });
+
   // Strike an item from the list
-  li.on("click", () => {
-    li.toggleClass("strike");
+  li.on('click', () => {
+    li.toggleClass('strike');
   });
 
   // Add a delete button
-  let crossOutButton = $('<div class="crossOutButton">&times;</div>');
+  let crossElement = '<div class="crossOutButton">&times;</div>';
+  let crossOutButton = $(crossElement);
   li.append(crossOutButton);
 
-  crossOutButton.on("click", () => {
-    li.addClass("delete");
+  crossOutButton.on('click', () => {
+    li.addClass('delete');
   });
 
   // Reorder items in the list
